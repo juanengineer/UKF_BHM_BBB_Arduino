e9ff2251ab728698f896f64e027edfa2108aeab2
juanengineer <82479357+juanengineer@users.noreply.github.com>
2021-11-09 21:25:18 -0600

    Add files via upload

---
+++ usb_spj/usb_spj.ino
@@ -0,0 +1,28 @@
+// This function is called once when the program starts. 
+void setup() {
+   // Choose a baud rate and configuration. 9600 
+   // Default is 8-bit with No parity and 1 stop bit
+   Serial.begin(9600, SERIAL_8N1);
+
+}
+
+// This function will loop as quickly as possible, forever.
+void loop() {
+   //char charIn;
+   Serial.println("led1on");
+   delay(100);
+   Serial.println("led1off");
+   delay(100);
+   /*
+   if(Serial.available()>0){          // A byte has been received
+      charIn = Serial.read();       // Read the character in from the BBB
+      delay(10);
+      if (charIn == 'y') Serial.println("u");         // Send the character back to the BBB 
+      if (charIn != 'y') Serial.println(charIn);
+     
+   }
+   */
+   //else charIn = 'x1';
+   //if(Serial.available()<=0) Serial.println("x2"); 
+   
+}
