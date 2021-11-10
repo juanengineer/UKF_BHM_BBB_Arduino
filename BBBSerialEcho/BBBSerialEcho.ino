c794b02f03400c302eabf72cdbfa5873e95676d2
juanengineer <82479357+juanengineer@users.noreply.github.com>
2021-11-09 20:26:31 -0600

    BBBSerialEcho3

--- BBBSerialEcho/BBBSerialEcho.ino
+++ BBBSerialEcho/BBBSerialEcho.ino
@@ -11,11 +11,12 @@ void loop() {
    char charIn;
    if(Serial.available()>0){          // A byte has been received
       charIn = Serial.read();       // Read the character in from the BBB
+      delay(10);
       if (charIn == 'y') Serial.println("u");         // Send the character back to the BBB 
       if (charIn != 'y') Serial.println(charIn);
-      delay(100);
+     
    }
-   else charIn = 'x1';
-   if(Serial.available()<=0) Serial.println("x2"); 
-   delay(100);
+   //else charIn = 'x1';
+   //if(Serial.available()<=0) Serial.println("x2"); 
+   delay(10);
 }
