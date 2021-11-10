8d4f39f7813a1e6752ed9694d5418abb5e611013
juanengineer <82479357+juanengineer@users.noreply.github.com>
2021-11-09 21:47:51 -0600

    Add files via upload

---
+++ post internship UKF tweaks MATLAB-transcoded-to-C/UKF_bhm6/codegen/lib/UKF_bhm3/codertarget_assembly_flags.mk
@@ -0,0 +1,3 @@
+ASFLAGS_ADDITIONAL = -march=armv7-a -mfloat-abi=hard -mfpu=neon
+STACK_SIZE = 200000
+TARGET_PKG_INSTALLDIR = C:/MATLAB/SUPPOR~1/R2015b/BEAGLE~2/toolbox/target/SUPPOR~1/BEAGLE~1/registry/..
---
+++ post internship UKF tweaks MATLAB-transcoded-to-C/UKF_bhm6/codegen/lib/UKF_bhm3/interface/_coder_UKF_bhm3_mex.h
@@ -0,0 +1,31 @@
+/*
+ * File: _coder_UKF_bhm3_mex.h
+ *
+ * MATLAB Coder version            : 3.0
+ * C/C++ source code generated on  : 05-Apr-2016 17:59:50
+ */
+
+#ifndef ___CODER_UKF_BHM3_MEX_H__
+#define ___CODER_UKF_BHM3_MEX_H__
+
+/* Include Files */
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tmwtypes.h"
+#include "mex.h"
+#include "emlrt.h"
+#include "_coder_UKF_bhm3_api.h"
+
+/* Function Declarations */
+void mexFunction(int32_T nlhs, mxArray *plhs[], int32_T nrhs, const mxArray
+                 *prhs[]);
+emlrtCTX mexFunctionCreateRootTLS(void);
+
+#endif
+
+/*
+ * File trailer for _coder_UKF_bhm3_mex.h
+ *
+ * [EOF]
+ */
